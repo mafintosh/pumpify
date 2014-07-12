3cca8e6f65eaab2addea4b07956b4ecea6f2f4d4
Mathias Buus <mathiasbuus@gmail.com>
2014-07-12 17:56:08 +0200

    always destroy one in the pipeline on destroy

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var pump = require('pump')
 var duplexify = require('duplexify')
 
+var Foo = function(proxy) {
+  this._proxy = proxy
+}
+
+Foo.prototype._e
+
 var pumpifier = function(duplex) {
   return function() {
     var streams = Array.isArray(arguments[0]) ? arguments[0] : Array.prototype.slice.call(arguments)
@@ -8,13 +14,18 @@ var pumpifier = function(duplex) {
     var last = streams[streams.length-1]
 
     var dup = duplex()
+    var w = first.writable ? first : null
+    var r = last.readable ? last : null
 
-    dup.setWritable(first.writable ? first : null)
-    dup.setReadable(last.readable ? last : null)
+    dup.setWritable(w)
+    dup.setReadable(r)
 
     var onclose = function() {
-      if (!first.writable && first.destroy) first.destroy()
-      if (!last.readable && last.destroy) last.destroy()
+      for (var i = 0; i < streams.length; i++) {
+        if (!streams[i].destroy) continue;
+        // we only need to destroy one. pump will care of the others
+        if (streams[i] !== r && streams[i] !== w) streams[i].destroy()
+      }
     }
 
     dup.on('close', onclose)
